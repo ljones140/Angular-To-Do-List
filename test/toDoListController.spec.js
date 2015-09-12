21d509ee9c86130cfdea030792817a23adeaed89
lewis <ljones140@gmail.com>
2015-09-12 17:59:18 +0100

    first commit, set up complete with angular karma and protractor, title test passing

---
+++ test/toDoListController.spec.js
@@ -0,0 +1,11 @@
+describe('ToDoListController', function () {
+
+  beforeEach(module('ToDoList'));
+
+  var ctrl;
+
+  beforeEach(inject(function ($controller) {
+    ctrl = $controller('ToDoListController');
+  }));
+
+});
